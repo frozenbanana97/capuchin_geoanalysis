cfe083223b2d020f972d93373a3242b022e0f121
frozenbanana97 <kylem0119@gmail.com>
2022-06-22 12:32:33 -0300

    create spatial function file for functions

---
+++ spatialFunctions.py
@@ -0,0 +1,23 @@
+import geopandas as gpd
+
+def scanSpatial(gdf, i, spatialCounter):
+    # counter =
+    # if counter == 1:
+    #     spatialCounter = 1
+    # Calculate the centroid of the group
+    centroid = gdf.dissolve().centroid
+
+    # Calculate distance of each point to the centroid of the group
+    for row in gdf['geometry']:
+        gdf.loc[:,'distCentr'] = gdf.distance(centroid[0])
+
+    # Create geodataframe for the area, perimeter, and polygon of each scan
+    area = gdf.dissolve().convex_hull
+    area = gpd.GeoDataFrame(gpd.GeoSeries(area))
+    area = area.rename(columns={0:'geometry'}).set_geometry('geometry')
+    area.loc[:,'area'] = area.area
+    area.loc[:,'perimeter'] = area.length
+    centroid.to_file('gpkgData/'+i[:-4]+'scans.gpkg', driver="GPKG", layer=i[:-4]+'_scan'+spatialCounter+'_centroid')
+    area.to_file('gpkgData/'+i[:-4]+'scans.gpkg', driver="GPKG", layer=i[:-4]+'_scan'+spatialCounter+'_zone')
+    gdf.to_file('gpkgData/'+i[:-4]+'scans.gpkg', driver="GPKG", layer=i[:-4]+'_scan'+spatialCounter)
+    spatialCounter = spatialCounter+1
