fad30b10e11acb29637977c9e030f26c6bbd1581
frozenbanana97 <kylem0119@gmail.com>
2023-02-14 23:08:15 -0500

    added tasks

--- analysis.py
+++ analysis.py
@@ -1,7 +1,16 @@
-# functions here to perform analysis after data creation
-# functions such as distance to next centroid, map creation, etc.
+# functions here to perform analysis after data creation, such as distance to next centroid, map creation, etc.
 # Things that require the gpkg's to already be created or for the data in a future scan to be created
-# heat map of monkey location
+# 
+# Scan Interval Length
+# Area overlap between scans
+# Day Range (whole day convex hull)
+# Month range
+# year range
+# show 25% of group leading the movememnt incluing age/sex
+# Home range
+# Automated map creation by day
+# Heat map of monkey location density
+
 
 
 # DISTANCE TO NEXT CENTROID
